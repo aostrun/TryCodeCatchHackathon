636e15387f2938f8a1970101ee09a7b91b96dd97
Andrijan Ostrun <andrijan.ostrun@gmail.com>
2018-05-24 01:01:28 +0200

    Blood Storage & Sample

--- django_template/blood_storage/views.py
+++ django_template/blood_storage/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from blood_storage.models import BloodStorage
+from blood_sample.models import BloodSample
 
 # Create your views here.
+
+
+@api_view(['GET',])
+def get_storage_sample_size(request, storage_id):
+    storage_samples = BloodSample.objects.all().filter(event__storage_id=storage_id)
+    #storage_samples.filter(co)
+
+
