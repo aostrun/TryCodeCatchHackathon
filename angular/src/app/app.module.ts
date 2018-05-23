3d2f6e91e7d314bf53a6cdb05ae9526da936a0fc
gosponMarko <marko.pavlic95@gmail.com>
2018-05-23 21:13:40 +0200

    Canvas ne iskace

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule }    from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AgmCoreModule } from '@agm/core';
-import { AngularOpenlayersModule} from 'angular2-openlayers';
 
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
@@ -26,8 +24,7 @@ import { RegisterComponent } from './register';
         BrowserAnimationsModule,
         ReactiveFormsModule,
         HttpClientModule,
-        routing,
-        AngularOpenlayersModule
+        routing
     ],
     declarations: [
         AppComponent,
