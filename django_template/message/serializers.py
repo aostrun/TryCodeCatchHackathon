981d6d604d02c10ee471e570fba861e2399bd305
Fran Celižić <fran.celizic@fer.hr>
2018-05-24 11:13:25 +0200

    message user detailes

--- django_template/message/serializers.py
+++ django_template/message/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from message.models import Message
+from custom_user.serializers import CustomRegisterSerializer
+
 
 
 class MessageSerializer(serializers.ModelSerializer):
+
+    message_from = CustomRegisterSerializer(source="message_from_user")
+    message_to = CustomRegisterSerializer(source="message_to_user")
+
     class Meta:
         model = Message
-        fields = ("id", "message_body", "message_from_user", "message_to_user", "is_read")
+        fields = ("id", "message_body", "message_from", "message_to", "is_read")
+
 
     def create(self, validated_data):
         """
